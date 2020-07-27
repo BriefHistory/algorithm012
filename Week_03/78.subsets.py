5248647150943f372f3f4ffc0b497b0bc4deafcb
ljhope <ljhope@gmail.com>
2020-07-27 20:36:57 +0800

    Add better solution of subset

--- Week_03/78.subsets.py
+++ Week_03/78.subsets.py
@@ -17,6 +17,20 @@ class Solution:
         for k in range(n + 1):
             backtrack()
         return output
+"""
+# 更优写法，但理解有难度
+ class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        
+        def helper(idx, tmp):
+            res.append(tmp)
+            for j in range(idx, n):
+                helper(j + 1,tmp + [nums[j]] )# tmp + [nums[j]] 这个会产生一个全新数组（copy出来的），然后传到下一次函数调用的参数。每次创建新的数组，所以就不需要进行reverse了
+        helper(0, [])
+        return res  
+ """
 # # 迭代
 # class Solution:
 #     def subsets(self, nums: List[int]) -> List[List[int]]:
